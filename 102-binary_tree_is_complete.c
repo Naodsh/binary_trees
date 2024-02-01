a87d6783d76b801df1b829337478f17ff0cd4418
Naodsh <naodadanech1977@gmail.com>
2024-02-01 04:57:30 -0800

    102-binary_tree_is_complete.c

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -38,3 +38,18 @@ int binary_tree_is_complete_helper(const binary_tree_t *tree,
 			binary_tree_is_complete_helper(
 				tree->right, 2 * index + 2, size, count + 1));
 }
+
+/**
+ * binary_tree_size - Measures the size of a binary tree
+ * @tree: Pointer to the root node of the tree
+ *
+ * Return: Size of the tree, 0 if tree is NULL
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	return (1 + binary_tree_size(tree->left) +
+			binary_tree_size(tree->right));
+}
