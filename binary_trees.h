8d62725a2d1be67e4013d575fa7b2ee5cc477473
Naodsh <naodadanech1977@gmail.com>
2024-02-01 04:23:24 -0800

    binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -44,7 +44,7 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 		const binary_tree_t *second);
 int binary_tree_is_ancestor(const binary_tree_t *ancestor,
-                const binary_tree_t *node) 
+                const binary_tree_t *node); 
 
 
 
