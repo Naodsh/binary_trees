a06e9d0780bb8c2381ebf3cbafea1bd558cc2080
Naodsh <naodadanech1977@gmail.com>
2024-02-01 04:43:19 -0800

    102-binary_tree_is_complete.c

---
+++ 102-binary_tree_is_complete.c
@@ -0,0 +1,40 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_complete - Checks if a binary tree is complete
+ * @tree: Pointer to the root node of the tree to check
+ *
+ * Return: 1 if the tree is complete, 0 otherwise
+ */
+int binary_tree_is_complete(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	return (binary_tree_is_complete_helper(
+				tree, 0, binary_tree_size(tree), 0));
+}
+
+/**
+ * binary_tree_is_complete_helper - Helper function to check if a binary
+ * tree is complete
+ * @tree: Pointer to the root node of the tree to check
+ * @index: Index of the current node (starting from 0)
+ * @size: Total number of nodes in the tree
+ * @count: Number of nodes traversed so far
+ *
+ * Return: 1 if the tree is complete, 0 otherwise
+ */
+int binary_tree_is_complete_helper(const binary_tree_t *tree,
+		size_t index, size_t size, size_t count)
+{
+	if (tree == NULL)
+		return (1);
+	if (index >= size)
+		return (0);
+
+	return (binary_tree_is_complete_helper(
+				tree->left, 2 * index + 1, size, count + 1) &&
+			binary_tree_is_complete_helper(
+				tree->right, 2 * index + 2, size, count + 1));
+}
--- binary_trees.h
+++ binary_trees.h
@@ -44,8 +44,12 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 		const binary_tree_t *second);
 int binary_tree_is_ancestor(const binary_tree_t *ancestor,
-                const binary_tree_t *node); 
+		const binary_tree_t *node);
 
+
+int binary_tree_is_complete(const binary_tree_t *tree);
+int binary_tree_is_complete_helper(const binary_tree_t *tree,
+		size_t index, size_t size, size_t count);
 
 
 void binary_tree_print(const binary_tree_t *);
